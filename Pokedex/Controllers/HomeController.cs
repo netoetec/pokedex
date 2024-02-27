6e52cb7a106321e52396b82db456d6e72e1b5179
netoetec <tagudelaconta123@gmail.com>
2024-02-27 20:39:48 -0300

    Incluido os tipos no HomeController e apresentação dos tipos na página Index. Criação da página de Details

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -16,15 +16,27 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        // Criar um objeto com o nome pokemnons do tipo Lista de Pokemon
+        // var pokemons = new List<Pokemon>();
+        // List<Pokemon> pokemons = new List<Pokemon>();
+        // List<Pokemon> pokemons = new();
+        List<Pokemon> pokemons = [];
         using (StreamReader leitor = new("Data\\pokemons.json"))
         {
             string dados = leitor.ReadToEnd();
-            List<Pokemon> pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
-            return View(pokemons);
+            pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
         }
+        List<Tipo> tipos = [];
+        using(StreamReader leitor = new("Data\\tipos.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
+        }
+        ViewData["Tipos"] = tipos;
+        return View(pokemons);
     }
 
-    public IActionResult Privacy()
+    public IActionResult Details(int id)
     {
         return View();
     }
